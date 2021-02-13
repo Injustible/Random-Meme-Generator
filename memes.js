5f3d8ad43d95d7a102791975ede0e0b76a1311bf
Injustible <78533502+Injustible@users.noreply.github.com>
2021-02-13 08:44:55 +0800

    version 0.5

--- memes.js
+++ memes.js
@@ -1,18 +1,21 @@
 let memes;
-let randMeme;
+let memetitle;
 
 function memeIt(memes) {
-	$.getJSON('https://meme-api.herokuapp.com/gimme')
+	$.getJSON('https://meme-api.herokuapp.com/gimme/PampamilyangPaoLUL')
 	.then(randMeme => {
+		console.log(randMeme)
+		memeTitle = randMeme.title
 		return randMeme.url;
 	})
 	.then(randMeme => {
-		console.log(randMeme)
+		console.log(memeTitle)
 		let img = document.createElement('img');
 		img.setAttribute('class', 'img-fluid');
 		img.src = randMeme;
 		document.getElementById("memesGoesHere").innerHTML = "";
 		document.getElementById("memesGoesHere").appendChild(img);
+		document.getElementById("title").textContent = `${memeTitle}`
 	});
 };
 
